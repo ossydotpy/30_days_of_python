0ed67df67ff279a78cd0b0d82b4ca7815c7812d9
noobmasterxi <mrali.osman.6@gmail.com>
2023-03-09 19:03:50 +0000

    day 19

--- exercises/day_19_json.py
+++ exercises/day_19_json.py
@@ -1,22 +1,14 @@
-# For this harder version of the project, the application needs to have the following functionality:
+# in this module, we are going to tweak our code from day 14 to save our files in JSON format.
+# we should also be able to read the data stored as a JSON.
 
-# Users should be able to add a book to their reading list by providing a book title, an author's name, a year of
-# publication, and whether or not the book has been read.
-# The program should store information about all of these books in a file called books.csv,
-# and this data should be stored in CSV format.
-# Users should be able to retrieve the books in their reading list, and these books should be printed out in a
-# user-friendly format.
-# Users should be able to search for a specific book by providing a book title.
-# Users should be able to mark a book as read by entering a book title. If there are multiple books with the same title,
-# you can just mark the first matching book as read.
-# Users should be able to delete books from their reading list by providing the book title for the book they want to
-# delete. Once again, you can just delete the first matching book.
-# Users should be able to select these options from a text menu, and they should be able to perform multiple operations
-# without restarting the program. You can see an example of a working menu in the post on while loops (day 8).# Day 14 
-
-# Project: Reading List (Hard Version)
+# we will only make changes to the:
+# retrieve function
+# add_book function
+# update function.
+import json
 
 def main():
+        createfile()
         while True:
                 try:
                         choice = context_menu()
@@ -52,44 +44,40 @@ def main():
 
                 except ValueError:
                         pass
-               
-
-def addBook():
-        with open('booksFile.csv','a') as books_file:
-                title = input('input book title:\n>>> ').strip().lower()
-                author = input("add author name:\n>>> ").lower().strip()
-                year_of_publication = input("add year of publication:\n>>> ").lower().strip()
-                readStatus = 'Unread'
-                book = f"{title}, {author}, {year_of_publication}, {readStatus}\n"
-
-                books_file.write(book.lower())
-        print('New Book Added')
 
 
 def retrieveBooks():
-        books = []
-        with open('./booksFile.csv','r') as books_file:
-                book_data = books_file.readlines()
-                for book in book_data:
-                        title,author,year,readStatus = book.strip().split(',')
-                        books.append({
-                                'title':title,
-                                'author':author,
-                                'year':year,
-                                'status':readStatus
-                        })
-        return books
+        with open('./booksFile.json','r') as books_file:
+            return json.load(books_file)         
+
+
+def addBook():
+        books = retrieveBooks()
+        
+        title = input('input book title:\n>>> ').strip().lower()
+        author = input("add author name:\n>>> ").lower().strip()
+        year_of_publication = input("add year of publication:\n>>> ").lower().strip()
+        readStatus = 'Unread'
+
+        books.append({
+        "title": title,
+        "author": author,
+        "year": year_of_publication,
+        "read": "Unread"
+        })
+
+        with open('booksFile.json','w') as books_file:
+                json.dump(books,books_file)
+        print('New Book Added')
 
 
 def showBooks(booksList):
         print()
         print(f'showing {len(booksList)} saved book(s)')
         print()
-        for index, book in enumerate(booksList,1):
-                details =(f"{index}). {book['title']} - ({book['year']}) by {book['author']} --{book['status']}")
-                print(details.title())
+        for book in booksList:
+                print("{title} - ({year}) by {author} --{read}".format(**book).title())
                 print()
-
 
 def searchBooks():
         books = retrieveBooks()
@@ -100,8 +88,7 @@ def searchBooks():
                 if query in book['title']:
                         matches.append(book)
         return matches
-
-      
+   
 # function called to perform an action[delete/mark] on a book                 
 def update_reading_list(function):
         books = retrieveBooks()
@@ -109,11 +96,9 @@ def update_reading_list(function):
 
         if matching_books:
                 function(books, matching_books[0])
-                with open("booksFile.csv", "w") as reading_list:
-                        for book in books:
-                                reading_list.write(f"{book['title']},{book['year']},{book['author']},{book['status']}\n")
-        else:
-                print("No matching results. Search for another book.\n")
+                with open("booksFile.json", "w") as reading_list:
+                        json.dump(books,reading_list)
+                        
 
 # mark as read
 def markRead(booksList,toUpdate):
@@ -145,5 +130,12 @@ def context_menu(): # function to display the menu items
             5. mark book as read
             6. exit \n>>> """).strip())
         return menu_item
+
+def createfile():
+        try:
+            with open('booksFile.json','x') as booksFile:
+                    json.dump([],booksFile)
+        except FileExistsError:
+                pass
 
 main()
